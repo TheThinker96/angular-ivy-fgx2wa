27f595d865006c9df6dd3b1c794691915071fee1
TheThinker96 <loubensluma8@gmail.com>
2021-01-25 17:14:02 -0500

    1 file modified, 2 added

---
+++ src/app/affichage-fonction/affichage-fonction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-affichage-fonction',
+  templateUrl: './affichage-fonction.component.html',
+  styleUrls: ['./affichage-fonction.component.css']
+})
+export class AffichageFonctionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { Route } from "@angular/compiler/src/core";
+import { AffichageFonctionComponent } from './affichage-fonction/affichage-fonction.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, AffichageFonctionComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
